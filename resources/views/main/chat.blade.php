11ab127ae9b0cb94561855696fa36e2473a1896c
IMAM SODIK <imamsodik00@gmail.com>
2025-08-20 00:30:50 +0700

    asdf

--- resources/views/main/chat.blade.php
+++ resources/views/main/chat.blade.php
@@ -690,8 +690,9 @@
 
         $(document).on("click", ".edit", function(e) {
             e.stopPropagation();
+            alert('test');
 
-            rn.addClass("opened");
+            // rn.addClass("opened");
 
             // var t = $(this).closest(".fn__chat_link");
             // return t.hasClass("opened") ? t.removeClass("opened") : t.addClass("opened")
